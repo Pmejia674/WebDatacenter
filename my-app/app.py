a26c9b7f92ab704be55241275319c6a0dc052167
Pmejia674 <paulmejiam651@gmail.com>
2025-12-07 01:19:53 -0500

    feat: Añadir soporte para servir robots.txt y mejorar la estructura de archivos JS ver proabr rendimiento

--- my-app/app.py
+++ my-app/app.py
@@ -1,5 +1,11 @@
-from flask import Flask
+from flask import Flask, send_from_directory
+import os
 
 app = Flask(__name__)
 application = app
 app.secret_key = '97110c78ae51a45af397b6534caef90ebb9b1dcb3380f008f90b23a5d1616bf1bc29098105da20fe'
+
+# Ruta para servir robots.txt
+@app.route('/robots.txt')
+def robots():
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'robots.txt', mimetype='text/plain')
